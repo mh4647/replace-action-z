27a7371633b8fdded897a145d424637c545f9005
mh4647 <92085925+mh4647@users.noreply.github.com>
2024-02-04 17:05:24 +0200

    Update to node 20

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 
 try {
-  const files = core.getInput('files');
-  const vars_string = core.getInput('replacements');
-  var filenames = files.replace(' ', '').split(',')
-  var vars = vars_string.split(',')
-  console.log('files l:'+ filenames.length)
-  for(var fi = 0; fi < filenames.length; fi++)
-  {
-    var filename = filenames[fi]
-    var fs = require('fs')
-    console.log('file1: '+ fi + ' '+filename)
-    fs.readFile(filename, 'utf8', function (err,data) {
+  const files = core.getInput("files");
+  const vars_string = core.getInput("replacements");
+  const filenames = files.replace(" ", "").split(",");
+  const vars = vars_string.split(",");
+  console.log("files l:" + filenames.length);
+  for (var fi = 0; fi < filenames.length; fi++) {
+    const filename = filenames[fi];
+    const fs = require("fs");
+    console.log("file1: " + fi + " " + filename);
+    fs.readFile(filename, "utf8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
-        var result = data
-        console.log(data)
-        for(var i = 0; i < vars.length; i++)
-        {
-          var firstEqual = vars[i].indexOf('=');
-    	  var key = vars[i].substr(0,firstEqual);
-    	  var value = vars[i].substr(firstEqual+1);
-    	  result = result.replace(key, value)
+        const result = data;
+        console.log(data);
+        for (let i = 0; i < vars.length; i++) {
+          const firstEqual = vars[i].indexOf("=");
+          const key = vars[i].substring(0, firstEqual);
+          const value = vars[i].substring(firstEqual + 1);
+          result = result.replace(key, value);
         }
-        console.log('file2: '+filename)
-        fs.writeFile(filename, result, 'utf8', function (err) {
-          if (err) 
-            console.log(err)
-          else 
-            console.log(result)
+        console.log("file2: " + filename);
+        fs.writeFile(filename, result, "utf8", function (err) {
+          if (err) console.log(err);
+          else console.log(result);
         });
       }
     });
